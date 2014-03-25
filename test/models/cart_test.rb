40e4fda60ff1308bcd398eb8d1caeb43dd0aa5fc
Ali Ismayilov <ali@ismailov.info>
2014-03-24 22:00:40 -0300

    Add unit test for Cart#add_product

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'add different products' do
+    @cart = carts(:one)
+
+    @cart.add_product products(:one)
+    @cart.add_product products(:ruby)
+
+    @cart.line_items.each do |line_item|
+      assert_equal line_item.quantity, 1
+    end
+  end
+
+  test 'add same product' do
+    @cart = carts(:one)
+
+    2.times { @cart.add_product products(:ruby) }
+
+    @cart.line_items.each do |line_item|
+      assert_equal line_item.quantity, 2
+    end
+  end
 end
