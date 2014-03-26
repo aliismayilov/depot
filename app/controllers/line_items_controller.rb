75063d647ef2a14303e9427b87f583ed1cd7b485
Ali Ismayilov <ali@ismailov.info>
2014-03-26 06:09:18 -0300

    Remove line_item 1 by 1

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -60,7 +60,11 @@ class LineItemsController < ApplicationController
   # DELETE /line_items/1
   # DELETE /line_items/1.json
   def destroy
-    @line_item.destroy
+    if @line_item.quantity > 1
+      @line_item.decrement! :quantity
+    else
+      @line_item.destroy
+    end
     respond_to do |format|
       format.html { redirect_to @line_item.cart }
       format.json { head :no_content }
